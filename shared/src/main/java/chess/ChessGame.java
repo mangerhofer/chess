cbf9d49dabd8999b92e1548601a887b002288632
McCall Angerhofer <mccall.angerhofer@gmail.com>
2024-05-10 16:08:37 -0600

    adding ChessGame test files to shared

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,8 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private TeamColor team;
+
     public ChessGame() {
 
     }
@@ -18,7 +20,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return team;
     }
 
     /**
@@ -27,7 +29,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.team = team;
     }
 
     /**
