24bf8caabe99c452ba3a2cdf911a64455e9b2599
McCall Angerhofer <mccall.angerhofer@gmail.com>
2025-02-21 23:06:25 -0700

    adding phase 3 files

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,26 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        try {
+            var port = 8080;
+            if (args.length >= 1) {
+                port = Integer.parseInt(args[0]);
+            }
+
+            port = new Server().run(port);
+            System.out.printf("Server started on port %d%n", port);
+            return;
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
+        System.out.println("""
+                Chess Server:
+                java ServerMain <port> [<dburl> <dbuser> <dbpassword> <dbname>]
+                """);
     }
 }
