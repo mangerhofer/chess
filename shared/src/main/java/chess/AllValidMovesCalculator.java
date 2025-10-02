26912f8e7812722b870202fa2b4cacca22c563c2
McCall Angerhofer <mccall.angerhofer@gmail.com>
2025-10-01 21:49:15 -0600

    adding some more code to allvalidmovescalculator

--- shared/src/main/java/chess/AllValidMovesCalculator.java
+++ shared/src/main/java/chess/AllValidMovesCalculator.java
@@ -85,4 +85,89 @@ public class AllValidMovesCalculator {
             allValidMovesArray.addAll(pieceMoves(newPos, piece, board));
         }
     }
+
+    public static boolean teamInCheck(ChessPosition kingPos, Collection<ChessMove> chessMoves) {
+        boolean inCheck = false;
+
+        for (ChessMove move : validMoves) {
+            ChessPosition endPos = move.getEndPosition();
+            int xEnd = endPos.getRow();
+            int yEnd = endPos.getColumn();
+            if (kingPos != null) {
+                int xKing = kingPos.getRow();
+                int yKing = kingPos.getColumn();
+                if (xEnd == xKing && yEnd == yKing) {
+                    inCheck = true;
+                    break;
+                }
+            }
+        }
+        return inCheck;
+    }
+
+    public static void tryMove(ChessMove move, ChessPosition kingPos, ChessPiece piece, ChessBoard board) {
+        ChessPosition startPos = move.getStartPosition();
+        ChessPosition endPos = move.getEndPosition();
+        ChessBoard copy = new ChessBoard(board);
+
+        board.addPiece(endPos, piece);
+        board.addPiece(startPos, null);
+
+        if (piece.getPieceType() == ChessPiece.PieceType.KING) {
+            if (AllValidMovesCalculator.teamInCheck(endPos, allValidMovesArray)) {
+                board.copyBoard(copy);
+            } else {
+                board.copyBoard(copy);
+                if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                    validWhiteMoves.add(move);
+                } else {
+                    validBlackMoves.add(move);
+                }
+            }
+        } else {
+            if (AllValidMovesCalculator.teamInCheck(kingPos, allValidMovesArray)) {
+                board.copyBoard(copy);
+            } else {
+                board.copyBoard(copy);
+                if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                    validWhiteMoves.add(move);
+                } else {
+                    validBlackMoves.add(move);
+                }
+            }
+        }
+    }
+
+    public static void tryMoveHelper(ChessMove move, ChessPosition king, ChessPiece piece, ChessBoard board) {
+        ChessBoard copy = new ChessBoard(board);
+        ChessPosition endPos = move.getEndPosition();
+        validMoves.clear();
+
+        tryMove(move, king, piece, board);
+        if (teamInCheck(endPos, allValidMovesArray)) {
+            board.copyBoard(copy);
+        } else {
+            board.copyBoard(copy);
+            if (!validMoves.contains(move)) {
+                validMoves.add(move);
+            }
+        }
+    }
+
+    public static Collection<ChessMove> moveHelper2(ChessPiece piece, ChessMove move, ChessPosition king, ChessBoard board) {
+        validMoves.clear();
+        if (piece.getPieceType() == ChessPiece.PieceType.KING) {
+            tryMoveHelper(move, king, piece, board);
+        } else {
+            if (teamInCheck(king, allValidMovesArray)) {
+                tryMove(move, king, piece, board);
+            } else {
+                tryMove(move, king, piece, board);
+                if (!validMoves.contains(move)) {
+                    validMoves.add(move);
+                }
+            }
+        }
+        return validMoves;
+    }
 }
